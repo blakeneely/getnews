6752e3d4b3010ec5b78b22f0811d382edcf2da08
Blake Neely <blakeneely21@gmail.com>
2019-12-02 01:45:17 -0600

    added notes controller

---
+++ controllers/notes.js
@@ -0,0 +1,32 @@
+// Set up dependencies
+const makeDate = require("../scripts/date");
+const Note = require("../models/Note");
+
+module.exports = {
+    get: function(data, callback){
+        Note.find({
+            _headlineID: data._id
+        }, callback);
+    },
+    save: function(data, callback){
+        let newNote = {
+            _headlineID: data._id,
+            date: makeDate(),
+            noteText: data.noteText
+        };
+        Note.create(newNote, function(err, doc){
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(doc);
+                callback(doc);
+            }
+        });
+    },
+    delete: function(data, callback){
+        Note.remove({
+            _id: data._id
+        }, callback);
+    }
+};
